705316d2d7d43515c156c929bfc90212762a2cad
ftc443h <sosmed443@gmail.com>
2023-04-11 22:23:57 +0700

    Akademi Fullstack Web Developer

---
+++ footer.php
@@ -0,0 +1,24 @@
+<!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Fatchan Muhammad Hakim</h3>
+      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="https://github.com/ftc443h" class="google-plus"><i class="bx bxl-github"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+      <div class="copyright"> NF Computer
+        &copy; Copyright <strong><span>Fatchan Muhammad Hakim</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: [license-url] -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/ -->
+        Fullstack Web Developer by <a href="https://learn.nurulfikri.com/">NF Computer</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
---
+++ resume.php
@@ -0,0 +1,109 @@
+<?php
+
+ include_once 'index.php';
+ include_once 'header.php';
+
+
+?>
+
+<header id="header" class="d-flex flex-column justify-content-center">
+
+    <nav id="navbar" class="navbar nav-menu">
+      <ul>
+        <li><a href="home.php?halaman=home" class="nav-link scrollto "><i class="bx bx-home"></i> <span>Home</span></a></li>
+        <li><a href="about.php?halaman=about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+        <li><a href="resume.php?halaman=resume" class="nav-link scrollto active"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+        <li><a href="portfolio.php?halaman=portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+        <li><a href="skill.php?halaman=skill" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Skill</span></a></li>
+      </ul>
+    </nav><!-- .nav-menu -->
+
+</header><!-- End Header -->
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h3 class="resume-title">Sumary</h3>
+            <div class="resume-item pb-0">
+              <h4>Fatchan Muhammad Hakim</h4>
+              <p><em id="countiner-em">Saya pernah mengikuti pelatihan untuk mengajar di kampus pelita bangsa, yang di sediakan oleh guru kami dan materi yang saya bawakan sama temen saya adalah tentang mikrotik. bukan hanya mengikuti pelatihan saja tapi saya juga pernah membuat 2 buku yang tentang mikrotik yaitu materi Bandwiwtdh (QOS), dan Firewall (Keamanan Jaringan). 
+                  selama membuat buku itu sampai 2 tahun, karena satu tahun itu fokus membahas tentang firewall (Keamanan), sedangkan satu tahun lagi itu fokus tentang QOS (Bandwidth). yang selama saya sekolah di Rabbaanii Islamic School.</em></p>
+              <ul>
+                <li>Cikarang Barat</li>
+                <li>(+62) 821-2513-9270</li>
+                <li>fatchanmuhammadhakim9@gmail.com</li>
+              </ul>
+            </div>
+
+            <h3 class="resume-title">Pendidikan</h3>
+            <div class="resume-item">
+              <h4>Mikrotik &amp; Cisco.</h4>
+              <h5>2017 - 2020</h5>
+              <p><em id="countiner-em">Rabbaanii Islamic School</em></p>
+              <p id="countiner-em">Cisco Systems, Inc. adalah sebuah konglomerat teknologi multinasional yang berkantor pusat di San Jose, California, Amerika Serikat. Cisco mengembangkan, memproduksi, dan menjual perangkat keras jaringan, perangkat lunak jaringan, peralatan telekomunikasi, serta produk dan jasa berteknologi tinggi lainnya.</p>
+            </div>
+            <div class="resume-item">
+              <h4>Web Developer &amp; Jaringan Komputer.</h4>
+              <h5>2020 - 2024</h5>
+              <p><em>Sekolah Tinggi Teknologi Terpadu Nurul Fikri</em></p>
+              <p>Jaringan komputer adalah sebuah jaringan yang dihubungkan secara elektronik antar komputer untuk saling berkomunikasi dengan bertukar data dan berbagi sumber daya satu sama lain. Tujuan dari jaringan komputer adalah agar setiap bagian dari jaringan komputer dapat meminta dan memberikan layanan.</p>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <h3 class="resume-title">PROFESSIONAL EXPERIENCE</h3>
+            <div class="resume-item">
+              <h4>Pengalaman</h4>
+              <ul>
+                <li>Membuat keamanan firewall menggunakan Pfsense di PT Wiraswasta Gemilang Indonesia</li>
+                <li>Membuat server local menggunakan FreeBSD di PT Wiraswasta Gemilang Indonesia</li>
+                <li>Pemateri training mikrotik (Dasar-Dasar Mikrotik)</li>
+                <li>Membuat buku tentang mikrotik (Firewall, Management Bandwitdh, Static Routing)</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Skill</h4>
+              <ul>
+                <li>Firewall</li>
+                <li>Tunneling & Vlan</li>
+                <li>ARP & Bridge & DHCP</li>
+                <li>Linux & Windows</li>
+                <li>Management Hotspot</li>
+                <li>HTML & CSS</li>
+                <li>Pemrograman PHP</li>
+                <li>OSPF & Routing</li>
+              </ul>
+            </div>
+              <div class="resume-item">
+                  <h4>Sertifikat</h4>
+                  <ul>
+                      <li>Dasar-Dasar DevOps | 15 - Juni - 2022</li>
+                      <li>Jaringan Komputer untuk Pemula | 19 - Juli - 2022</li>
+                      <li>Dasar Visualisasi Data | 27 - Juli - 2022</li>
+                      <li>Pemrograman Python | 15 - Agustus - 2022</li>
+                  </ul>
+              </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/typed.js/typed.min.js"></script>
+<script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
